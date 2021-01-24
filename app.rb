75b6e0a53d002803c337e47f7b50487d5fcfce03
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-01-24 11:46:17 -0500

    commit tree 8ca545625e438bd37a49145dba963c018256d3b5

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Emrich"
+    end
+
+    get '/hometown' do
+        "My hometown is New York City"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Drugs You Should Try it by Travis Scott"
+    end
 end
